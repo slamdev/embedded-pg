f7d298ae9495fd26605d15a43d5c4707344c1a3e
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2019-01-28 19:05:00 +1100

    add java8 implementation

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.github.slamdev.embedded.pg {
-    requires google.options;
-    requires otj.pg.embedded;
-    requires slf4j.api;
-    requires java.sql;
-    exports com.github.slamdev.embedded.pg;
-}
